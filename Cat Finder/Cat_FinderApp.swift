f94b59e2814e6b006610e050e2716bfd6fdd1604
TETRA2000 <sagittariusm25@gmail.com>
2020-09-27 20:45:54 +0900

    Initial Commit

---
+++ Cat Finder/Cat_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_FinderApp.swift
+//  Cat Finder
+//
+//  Created by Takahiko Inayama on 2020/09/27.
+//
+
+import SwiftUI
+
+@main
+struct Cat_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cat Finder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cat Finder
+//
+//  Created by Takahiko Inayama on 2020/09/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
